aa9a8c087769a786356f7ac17bc931447d999968
Neves888 <helen.vneves@gmail.com>
2023-03-28 16:48:25 -0300

    teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+    assert encrypt_message("abcdef", 3) == "cba_fed"
+    assert encrypt_message("cerveja", 8) == "ajevrec"
