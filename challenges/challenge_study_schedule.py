c79359d1f33e97cb55de39459d86cfffba8e6076
Neves888 <helen.vneves@gmail.com>
2023-03-28 16:14:56 -0300

    add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    result = 0
+    try:
+        for student_time in permanence_period:
+            if student_time[0] <= target_time <= student_time[1]:
+                result += 1
+        return result
+    except TypeError:
+        return None
