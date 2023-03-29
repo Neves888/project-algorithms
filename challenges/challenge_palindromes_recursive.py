b2a0223e18cc5d19ddd767cee92de1cb5e30166f
Neves888 <helen.vneves@gmail.com>
2023-03-29 10:24:15 -0300

    add req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not len(word):
+        return False
+    elif word[low_index] != word[high_index]:
+        return False
+    elif high_index <= low_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
